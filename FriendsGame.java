40cc00f776e5773c318275f381e0fdef23269acc
Brahmareddy <brahma.reddy@softsuave.com>
2023-04-25 16:00:38 +0530

    logical question

--- FriendsGame.java
+++ FriendsGame.java
@@ -1,7 +1,46 @@
 package p1;
 import java.util.Random;
+/*
+ You are playing the Bulls and Cows game with your friend.
+
+You write down a secret number and ask your friend to guess what the number is. When your friend makes a guess, you provide a hint with the following info:
+
+The number of "bulls", which are digits in the guess that are in the correct position.
+The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position. Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
+Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
+
+The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
+
+ 
+
+Example 1:
+
+Input: secret = "1807", guess = "7810"
+Output: "1A3B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1807"
+  |
+"7810"
+Example 2:
+
+Input: secret = "1123", guess = "0111"
+Output: "1A1B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1123"        "1123"
+  |      or     |
+"0111"        "0111"
+Note that only one of the two unmatched 1s is counted as a cow since the non-bull digits can only be rearranged to allow one 1 to be a bull.
+ 
+
+Constraints:
+
+1 <= secret.length, guess.length <= 1000
+secret.length == guess.length
+secret and guess consist of digits only.
+ */
 import java.util.Scanner;
 public class FriendsGame {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,6 +52,7 @@ public class FriendsGame {
 			int n=r.nextInt(10);
 			sb.append(Integer.toString(n));
 		}
+		System.out.println(sb);
 		System.out.println("enter the 4 letter String containg 4 letters");
 		String str=s.nextLine();
 		int bu=0,co=0;
--- geeksforgeeks19042023.java
+++ geeksforgeeks19042023.java
@@ -1,27 +1,79 @@
 package p1;
+/*
+ * There are N rooms in a straight line in Geekland State University's hostel, you are given a binary string S of length N where S[i] = '1' represents that there is a wifi in ith room or S[i] = '0' represents no wifi. Each wifi has range X i.e. if there is a wifi in ith room then its range will go upto X more rooms on its left as well as right. You have to find whether students in all rooms can use wifi.
+
+Example 1: 
+
+Input:
+N = 3, X = 0
+S = "010"
+Output:
+0
+Explanation: 
+Since the range(X)=0, So Wifi is only 
+accessible in second room while 1st & 3rd
+room have no wifi.
+Example 2: 
+
+Input:
+N = 5, X = 1
+S = "10010"
+Output:
+1
+Explanation: 
+Index 0 : Wifi is available
+Index 1 : Since range of 0th Index is 1
+          so, here wifi will be available.
+Index 2 : Since range of 3rd Index is 1
+          so, here also wifi available.
+Index 3 : Wifi is available
+Index 4 : here range of 3rd Index is available.
+So all the rooms have wifi, so return true.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function wifiRange( ) which takes integer N, string S and integer X as input parameters and returns true if students in all rooms can use wifi or false otherwise.
+
+Expected Time Complexity:O(N)
+Expected Space Complexity:O(1)
+
+Constraints:
+1 ≤ N ≤ 106
+0 ≤ X ≤ 106
+ */
 
 public class geeksforgeeks19042023 {
 	public static void main(String[] args) {
-		int n = 3, r = 1,count=0;
 		StringBuffer s = new StringBuffer("10010");
-		for(int i=1;i<n-1;i++)
-		{
-			char c=s.charAt(i);
-			if(c=='1')
-			{
-				
-			}
-		}//for loop
+		int n = s.length(), r = 1, count = 0;
+		if (r > n)
+			r = n;
+		for (int i = 0; i < n; i++) {
 
-		for(int i=0;i<s.length();i++)
-		{
-			if(s.charAt(i)=='1')
+			if (s.charAt(i) == '1') {
+
+				int bf = i - r, af = i + r;
+				if (bf < 0)
+					bf = 0;
+				if (af > n - 1)
+					af = n - 1;
+				for (int j = bf; j < i; j++) {
+					s.replace(j, j + 1, "1");
+				}
+				for (int j = af; j < af + r; j++) {
+					s.replace(j, j + 1, "1");
+
+				}
+				i = i + r;
+			}
+		} // for loop
+		System.out.println(s);
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '1')
 				count++;
 		}
-		if(count==n)
-		{
+		if (count == n || (r >= n && count > 0)) {
 			System.out.println(1);
-		}else
+		} else
 			System.out.println(0);
 	}
 
